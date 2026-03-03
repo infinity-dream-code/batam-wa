00e49a351151883565efa0b7ebcc649d3d6e1eb4
gatotpur <yulius.gtt@gmail.com>
2026-03-03 14:38:25 +0700

    add MySQL configuration for WA database

--- config/database.php
+++ config/database.php
@@ -83,6 +83,26 @@ return [
             ]) : [],
         ],
 
+        'mysql_wa' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_WA_HOST', '127.0.0.1'),
+            'port' => env('DB_WA_PORT', '3306'),
+            'database' => env('DB_WA_DATABASE', 'forge'),
+            'username' => env('DB_WA_USERNAME', 'forge'),
+            'password' => env('DB_WA_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
